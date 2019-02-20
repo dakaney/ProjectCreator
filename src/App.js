0dce4d759949cda3cea2877226c8a71b6a1df713
Dakaney <wayman_ng@yahoo.com>
2019-02-20 15:10:02 -0800

    added in modular project summary for project list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 
 class App extends Component {
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
-import React from 'react';
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted By Dakaney</p>
-                    <p className="grey-text">12th Febuary, 1pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted By Dakaney</p>
-                    <p className="grey-text">12th Febuary, 1pm</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted By Dakaney</p>
-                    <p className="grey-text">12th Febuary, 1pm</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
