c8a012921c30eb8e8e68b4a6c2afcc4cd7efeafb
Dakaney <wayman_ng@yahoo.com>
2019-02-20 16:50:14 -0800

    added redux store for projects

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'hello', content: 'stuff about this project'},
+        {id: '2', title: 'world', content: 'stuff about this project'},
+        {id: '3', title: 'bye', content: 'stuff about this project'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
