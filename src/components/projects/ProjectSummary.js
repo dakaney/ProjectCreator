539f28b590bcc45880924dd96abdacc8249bfda1
Dakaney <wayman_ng@yahoo.com>
2019-02-24 13:44:29 -0800

    fixed project creation - showing correct info

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>Posted By Dakaney</p>
+                <p>Posted By {project.authorFirstName} {project.authorLastName}</p>
                 <p className="grey-text">12th Febuary, 1pm</p>
             </div>
         </div>
