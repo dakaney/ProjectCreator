b9a292e1b94a73da58faf83a2f1bef3e09c36dac
Dakaney <wayman_ng@yahoo.com>
2019-02-24 11:28:17 -0800

    only renders page after firebase auth determined

--- src/index.js
+++ src/index.js
@@ -14,11 +14,13 @@ import fbConfig from './config/fbConfig'
 const store = createStore(rootReducer, 
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-        reactReduxFirebase(fbConfig),
+        reactReduxFirebase(fbConfig, {attachAuthIsReady: true}),
         reduxFirestore(fbConfig)
     )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    serviceWorker.unregister();
+})
 
-serviceWorker.unregister();
