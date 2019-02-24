7be0a913d36e9e3ac7adec943ab28305a588e9a5
Dakaney <wayman_ng@yahoo.com>
2019-02-24 13:28:09 -0800

    added in profile display to navbar

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -8,7 +8,7 @@ const SignedInLinks = (props) => {
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
             <li><a onClick={props.signOut}>Log Out</a></li>
-            <li><NavLink to='/'className='btn btn-floating blue darken-3'>WN</NavLink></li>
+            <li><NavLink to='/'className='btn btn-floating blue darken-3'>{props.profile.initials}</NavLink></li>
         </ul>
     )
 }
