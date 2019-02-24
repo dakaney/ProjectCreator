d784067cda56a1d13216d5027f74d2569ff9ce99
Dakaney <wayman_ng@yahoo.com>
2019-02-24 13:15:21 -0800

    added signup features

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { signUp } from '../../store/actions/authActions'
 
 class SignUp extends Component {
     state = {
@@ -16,10 +17,10 @@ class SignUp extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        this.props.signUp(this.state);
     }
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to='/' />
     return (
       <div className="container">
@@ -34,15 +35,18 @@ class SignUp extends Component {
                 <input type="password" id="password" onChange={this.handleChange}/>
             </div>
             <div className="input-field">
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" id="lastName" onChange={this.handleChange}/>
-            </div>
-            <div className="input-field">
                 <label htmlFor="firstName">First Name</label>
                 <input type="text" id="firstName" onChange={this.handleChange}/>
             </div>
             <div className="input-field">
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" id="lastName" onChange={this.handleChange}/>
+            </div>
+            <div className="input-field">
             <button className="btn blue darken-3 z-depth-0">Submit</button>
+            <div className="red-text center">
+                {authError? <p>{ authError }</p> : null}
+            </div>
             </div>
         </form>
       </div>
@@ -52,8 +56,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
     }
 }
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
